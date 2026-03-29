01a53362bc9f39b87450e2cd0b587f9967a87004
Joseph J Kane III <j.kane.third@gmail.com>
2026-03-29 16:16:20 -0400

    Add an app icon

---
+++ assets/LICENSE_EMOJI.md
@@ -0,0 +1,10 @@
+The application icon (u1f989_u1f50e.png) was created using Google Emoji Kitchen.
+Emoji Kitchen uses emojis from the Noto Emoji project, which is licensed under the Apache License 2.0.
+
+Apache License
+Version 2.0, January 2004
+http://www.apache.org/licenses/
+
+TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+...
+(Simplified due to length, referring to full Apache 2.0 license)
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:ebird_generator/services/bird_classifier.dart';
 import 'package:ebird_generator/ui/main_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await BirdClassifier().unloadModel();
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/LICENSE_EMOJI.md');
+    yield LicenseEntryWithLineBreaks(['Emoji Kitchen Icon'], license);
+  });
+
   runApp(const MyApp());
 }
 
