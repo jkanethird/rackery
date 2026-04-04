67e7895b4eb69428b974bd87e652ac60785d1ede
Joseph J Kane III <j.kane.third@gmail.com>
2026-04-03 22:06:30 -0400

    Tie accent colors to system accent color

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:system_theme/system_theme.dart';
 import 'package:ebird_generator/ui/main_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemTheme.accentColor.load();
 
   LicenseRegistry.addLicense(() async* {
-    final emojiLicense =
-        await rootBundle.loadString('assets/LICENSE_EMOJI.md');
+    final emojiLicense = await rootBundle.loadString('assets/LICENSE_EMOJI.md');
     yield LicenseEntryWithLineBreaks(['Emoji Kitchen Icon'], emojiLicense);
 
-    final bioClipLicense =
-        await rootBundle.loadString('assets/LICENSE_BIOCLIP.md');
+    final bioClipLicense = await rootBundle.loadString(
+      'assets/LICENSE_BIOCLIP.md',
+    );
     yield LicenseEntryWithLineBreaks(['BioCLIP Vision Model'], bioClipLicense);
   });
 
@@ -24,17 +26,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eBird Checklist Generator',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueGrey,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
-      themeMode: ThemeMode.dark,
-      home: const MainScreen(),
+    return SystemThemeBuilder(
+      builder: (context, accent) {
+        return MaterialApp(
+          title: 'eBird Checklist Generator',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: accent.accent,
+              brightness: Brightness.dark,
+              surface: HSLColor.fromColor(
+                accent.darkest,
+              ).withLightness(0.025).toColor(),
+            ),
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.dark,
+          home: const MainScreen(),
+        );
+      },
     );
   }
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_onnxruntime/flutter_onnxruntime_plugin.h>
+#include <system_theme/system_theme_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterOnnxruntimePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterOnnxruntimePlugin"));
+  SystemThemePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SystemThemePlugin"));
 }
